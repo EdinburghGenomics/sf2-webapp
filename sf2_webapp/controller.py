5b6dbd16b7c2c6e0151eae916b711c147b663d2b
Hywel Dunn-Davies <hywel.dunn-davies@ed.ac.uk>
2019-06-26 11:38:35 +0100

    Added code to render static files from the build directory

--- sf2_webapp/controller.py
+++ sf2_webapp/controller.py
@@ -1,24 +1,37 @@
 """Controller module for sf2 web application"""
 
+import os
+
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 
 
+settings = {
+    'debug': True
+}
+
+    
+# Request handlers -----
+    
 class MainHandler(tornado.web.RequestHandler):
     """Class to handle requests to the top level URL"""
 
     def get(self):
-        self.write("Online SF2")
+        self.render("../build/index.html")
+        
 
+# Run function -----
 
 def run(port):
-    """Run the server and listen on the specified port"""
+    """Runs the server and listens on the specified port"""
 
-    application = tornado.web.Application([(r"/", MainHandler)])
+    handlers = [
+        (r'/', MainHandler),
+        (r'/(favicon\.ico)', tornado.web.StaticFileHandler, {'path': 'build'})
+    ]
+
+    application = tornado.web.Application(handlers, **settings)
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(port)
     tornado.ioloop.IOLoop.current().start()
-
-
-
