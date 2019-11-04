ee69c83ef35003b0cf74d81a45ab4674e58dfbdc
Hywel Dunn-Davies <hywel.dunn-davies@ed.ac.uk>
2019-11-04 17:09:25 +0000

    Added build directories

---
+++ client/project_setup/build/precache-manifest.005f9473ae8321260c90b4f67b1a4d2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0df40d77c5032b8ab805e645d48f4157",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b992a3cd85008d76ad7",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "e6da674ef49781fc0dea",
+    "url": "/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "6b992a3cd85008d76ad7",
+    "url": "/static/js/2.5b7a04b0.chunk.js"
+  },
+  {
+    "revision": "e6da674ef49781fc0dea",
+    "url": "/static/js/main.81a14c69.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
